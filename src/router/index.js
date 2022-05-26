d3b58a81c474c2f9c8a23638a00b63d801673cf9
Claire <a115487@hotmail.com>
2022-05-26 10:15:27 +0800

    fix: router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import store from '@store';
+import Home from '@/views/Home';
 
 const routes = [
 	{
@@ -16,7 +17,7 @@ const routes = [
 			{
 				name: 'Home',
 				path: '/',
-				component: () => import('@/views/Home')
+				component: Home
 			},
 			{
 				name: 'Following',
